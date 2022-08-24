05703af17a2dbbb0e8632a7eb86cfea38a4ece83
Manuela Rios <manuela.rios.silva@gmail.com>
2022-08-24 17:33:07 -0300

    Praticando JavaScript

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,6 +1,7 @@
 let nome = "Manuela Rios"
 let email = "manuela.rios.silva@gmail.com"
-let profissao = "Estudante de engenharia de software"
+profissao = "Estudante de engenharia de software"
 let idade = 18
 let lugar = "Egito"
 
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos.\nEstamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu e-mail é ${email}? Ficamos no aguardo.\nMuito obrigado e boa viagem! :D`)
